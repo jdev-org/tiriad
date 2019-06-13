6ecdf7f45471d5d1cf61e3266f144e9cecee43e3
Gaetanbrl <gaetan.bruel@jdev.fr>
2019-06-13 19:38:10 +0200

    Popup component to display click object from all layers

---
+++ src/components/popup.vue
@@ -0,0 +1,58 @@
+<!-- eslint-disable -->
+<template>
+    <!-- Popup -->
+    <div :id="id" class="ol-popup">
+      <div id="popup-content"></div>
+    </div>
+</template>
+<script>
+/* eslint-disable */
+export default {
+    name: 'overLaysPopup',
+    props: ['id'],
+}
+</script>
+    <style>
+      .ol-popup {
+        position: absolute;
+        background-color: white;
+        -webkit-filter: drop-shadow(0 1px 4px rgba(0,0,0,0.2));
+        filter: drop-shadow(0 1px 4px rgba(0,0,0,0.2));
+        padding: 15px;
+        border-radius: 10px;
+        border: 1px solid #cccccc;
+        bottom: 12px;
+        left: -50px;
+        min-width: 280px;
+      }
+      .ol-popup:after, .ol-popup:before {
+        top: 100%;
+        border: solid transparent;
+        content: " ";
+        height: 0;
+        width: 0;
+        position: absolute;
+        pointer-events: none;
+      }
+      .ol-popup:after {
+        border-top-color: white;
+        border-width: 10px;
+        left: 48px;
+        margin-left: -10px;
+      }
+      .ol-popup:before {
+        border-top-color: #cccccc;
+        border-width: 11px;
+        left: 48px;
+        margin-left: -11px;
+      }
+      .ol-popup-closer {
+        text-decoration: none;
+        position: absolute;
+        top: 2px;
+        right: 8px;
+      }
+      .ol-popup-closer:after {
+        content: "✖";
+      }
+    </style>
