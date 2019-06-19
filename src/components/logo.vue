1a81d1e9b8a779bb5e0bcd224f421c3174e548bb
Gaetanbrl <gaetan.bruel@jdev.fr>
2019-06-19 09:59:43 +0200

    Logo more responsive

--- src/components/logo.vue
+++ src/components/logo.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable -->
 <template>
-    <div id="logo-brand" class="logo-org col-8 col-xs-5 col-sm-2 col-md-2">
+    <div id="logo-brand" class="logo-org col-4 col-xs-6 col-sm-2 col-md-2 pr-0">
         <a href="http://distillerie-vercors.com">
             <img src="http://distillerie-vercors.com/wp-content/uploads/2018/05/Distillerie_logo_brun_corail-e1527681979704.png">
         </a>
