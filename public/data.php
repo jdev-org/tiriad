7a2b05aa3856d134ad4c60b1ff42de71177c8cf3
Gaetanbrl <gaetan.bruel@jdev.fr>
2019-06-25 14:13:03 +0200

    chnge comment

--- public/data.php
+++ public/data.php
@@ -1,10 +1,12 @@
 <?php
-// get filename
+//get filename
 $fichier=$_POST['filename'];
+//get json content
 $geojsonStr=$_POST['content'];
-$destFolder="/tmp/upload";
-// get filecontent
+//get file content
 $geojson = file_get_contents('php://input');
+//set output folder
+$destFolder="/tmp/upload";
 //save to disk
 file_put_contents($destFolder . DIRECTORY_SEPARATOR .$fichier, $geojsonStr);
 header('Content-type: application/json',true);
