85eeb83089c3276efbdee558bff4db4d6f0f4887
Gaetanbrl <gaetan.bruel@jdev.fr>
2019-06-25 16:15:41 +0200

    clean code style

--- src/main.js
+++ src/main.js
@@ -1,27 +1,27 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 import Vue from 'vue';
-import Vuex from 'vuex'
+import Vuex from 'vuex';
 import VueLayers from 'vuelayers';
-import Buefy from 'buefy'
-import Papa from 'papaparse'
-import 'jquery'
-import 'popper.js'
-import 'bootstrap'
+import Buefy from 'buefy';
+import Papa from 'papaparse';
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
 import App from './App.vue';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'vuelayers/lib/style.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { store } from './store/store.js'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { store } from './store/store.js';
 
 Vue.use(VueLayers);
-Vue.use(Buefy)
+Vue.use(Buefy);
 Vue.use(Papa);
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 window.$ = require('jquery');
 window.JQuery = require('jquery');
 Vue.config.productionTip = false;
