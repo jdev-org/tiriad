93221fbb5469da87302ae18c1e95034f21dc1dca
Gaetanbrl <gaetan.bruel@jdev.fr>
2019-06-25 14:11:21 +0200

    Fix semilicon

--- public/getData.php
+++ public/getData.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type: application/json");
 
-$fileName = $_POST['filename']
+$fileName = $_POST['filename'];
 $strJsonFileContents = file_get_contents("/tmp/upload/".$fileName);
-echo $strJsonFileContents
+echo $strJsonFileContents;
 ?>
