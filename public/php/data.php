429106f7662196859e55a7cfa25f4aae20220d11
Gaetanbrl <gaetan.bruel@jdev.fr>
2019-06-26 16:31:29 +0200

    Fix php

--- public/php/data.php
+++ public/php/data.php
@@ -4,9 +4,9 @@ $fichier=$_POST['filename'];
 //get json content
 $geojsonStr=$_POST['content'];
 //set output folder
-$destFolder="/tmp/data/layers";
+$destFolder="../data/layers";
 //save to disk
 file_put_contents($destFolder . DIRECTORY_SEPARATOR .$fichier, $geojsonStr);
 header('Content-type: application/json',true);
-echo '{"success":true, "filepath":"'.$fichier.'", "path":'.$destFolder. DIRECTORY_SEPARATOR .$fichier.'}';
+echo '{"success":true, "filepath":"'.$fichier.'", "geojsonString":'.$geojsonStr.'}';
 ?>
--- public/php/getData.php
+++ public/php/getData.php
@@ -2,7 +2,7 @@
 header("Content-Type: application/json");
 
 $fileName = $_POST['filename'];
-$folder="/tmp/data/layers";
+$folder="../data/layers";
 $strJsonFileContents = file_get_contents($folder. DIRECTORY_SEPARATOR .$fileName);
-echo '{"filepath":"'$folder.$fileName.'", "path":'.$folder. DIRECTORY_SEPARATOR .$fileName.'}';
+echo '{"file":"'$fileName.'", "path":"'.$folder. DIRECTORY_SEPARATOR .$fileName.'"}';
 ?>
