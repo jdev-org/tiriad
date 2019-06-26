689354140025575509ad6481140803b4ffcf0bc8
Gaetanbrl <gaetan.bruel@jdev.fr>
2019-06-26 15:49:39 +0200

    update php

--- public/php/getData.php
+++ public/php/getData.php
@@ -4,5 +4,5 @@ header("Content-Type: application/json");
 $fileName = $_POST['filename'];
 $folder="../data/layers";
 $strJsonFileContents = file_get_contents($folder. DIRECTORY_SEPARATOR .$fileName);
-echo $strJsonFileContents;
+echo '{"content":'.$strJsonFileContents.'}';
 ?>
