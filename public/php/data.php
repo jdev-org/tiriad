6e3265be205d8b869aaa4c16e0decfce967645ed
Gaetanbrl <gaetan.bruel@jdev.fr>
2019-06-26 15:23:49 +0200

    simplify file and save to correct folder

--- public/php/data.php
+++ public/php/data.php
@@ -3,12 +3,9 @@
 $fichier=$_POST['filename'];
 //get json content
 $geojsonStr=$_POST['content'];
-//get file content
-$geojson = file_get_contents('php://input');
 //set output folder
-$destFolder="/tmp/upload";
+$destFolder="../data/layers";
 //save to disk
-file_put_contents($destFolder . DIRECTORY_SEPARATOR .$fichier, $geojsonStr);
 header('Content-type: application/json',true);
 echo '{"success":true, "filepath":"'.$fichier.'", "geojsonString":'.$geojsonStr.'}';
 ?>
