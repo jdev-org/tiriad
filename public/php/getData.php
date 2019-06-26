9a047bd32a12a9f29543bc79a590b26717bd41a4
Gaetanbrl <gaetan.bruel@jdev.fr>
2019-06-26 15:56:30 +0200

    update php

--- public/php/getData.php
+++ public/php/getData.php
@@ -5,4 +5,5 @@ $fileName = $_POST['filename'];
 $folder="../data/layers";
 $strJsonFileContents = file_get_contents($folder. DIRECTORY_SEPARATOR .$fileName);
 echo '{"content":'.$strJsonFileContents.'}';
+echo '{"filepath":"'$folder.$fileName.'", "path":'.$folder. DIRECTORY_SEPARATOR .$fileName.'}';
 ?>
