0e139ba089958cf24fc51e77081e5ae7e81257dd
Gaetanbrl <gaetan.bruel@jdev.fr>
2019-07-01 17:55:08 +0200

    Fix undefined app

--- src/components/vuemap.vue
+++ src/components/vuemap.vue
@@ -121,7 +121,7 @@ export default {
               format: 'GEOJSON',
               url: file.path,
               name: name,
-              style: app.$store.state.style.featuresStyle(params.format)
+              style: app.$store.getters.getStyle.featuresStyle(params.format)
             }
             let newLayer = app.createLayer(layer);            
             app.$store.state.map.addLayer(newLayer);
