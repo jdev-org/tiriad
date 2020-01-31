1ecb2ff9a134f348183c942a65c47cf7bf9e9d55
Gaetanbrl <gaetan.bruel@jdev.fr>
2020-01-31 16:56:28 +0100

    Add const to only know path of data without serveur path

--- public/srv/getLayers.php
+++ public/srv/getLayers.php
@@ -4,22 +4,22 @@ require 'const.php';
 
 // define layers array
 $layers = array();
-
+$SAVE_PATH = SERVER_PATH.REQUEST_PATH;
 // check if dir path is a directory
-if(is_dir(SAVE_PATH)){
+if(is_dir($SAVE_PATH)){
     // get files from dir
-    if($dh = opendir(SAVE_PATH)){
+    if($dh = opendir($SAVE_PATH)){
         // tant que le fichier trouvé n'est pas un répertoire
         while(($file = readdir($dh)) != false){
             if($file != "." and $file != ".." and !is_dir($file)){
                 // read files infos
-                $path = SAVE_PATH. DIRECTORY_SEPARATOR .$file;
+                $path = $SAVE_PATH. DIRECTORY_SEPARATOR .$file;
                 //TODO only for geojson, kml
                 if(file_get_contents($path) != '') {
                     // add files info to array
                     $layers[] = array(
                         'name' => $file,
-                        'path' => SAVE_PATH. DIRECTORY_SEPARATOR .$file
+                        'path' => $SAVE_PATH. DIRECTORY_SEPARATOR .$file
                         //'content' => file_get_contents($path)
                     );
                 }
